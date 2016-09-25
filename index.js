884737725f02b68da4f10d7ea46f873ad35a8272
wemlion <angusfu1126@qq.com>
2016-09-25 20:30:40 +0800

    fix ext support

--- index.js
+++ index.js
@@ -2,11 +2,12 @@
 module.exports = function (cooking) {
   // add loader
   cooking.add('loader.yaml', {
-    test: /\.yaml$/,
+    test: /\.ya?ml$/i,
     exclude: /node_modules|bower_components/,
     loaders: ['json-loader', 'yaml-loader']
   });
 
   // add extension
   cooking.config.resolve.extensions.push('.yaml');
+  cooking.config.resolve.extensions.push('.yml');
 };
